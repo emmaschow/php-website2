1f60cbf49c700f4878f23381ab14ce60f7a84f79
emmaschow <117453402+emmaschow@users.noreply.github.com>
2022-11-30 02:31:43 -0500

    Update psyc-250.php

--- psyc-250.php
+++ psyc-250.php
@@ -1,11 +1,11 @@
 <?php
 $pageTitle = 'PSYC 250';
-$bkgdImage = 'child.jpg';
+$bkgdImage = 'kid.jpg';
 include 'includes/header.php'; // Don't change this.
 ?>
 <?php include 'includes/class-nav.php'; ?>
 <h1>PSYCH 250</h1>
-
+<a href="https://sakai.unc.edu/portal/site/572a8196-1653-4e68-9b66-413e0f14c4d3"> PSYC 270</a>
 <p>This class examines the basics of child development.</p>
 
 <?php include 'includes/footer.php'; // Don't Change this
